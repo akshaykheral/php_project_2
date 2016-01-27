a7c70ce5b6d44f02776974e4d0e0f88ab2237a1e
akshaykheral <akshay.kheral@yahoo.com>
2016-01-27 16:25:28 +0530

    first commit

---
+++ uploading_pdf.php
@@ -0,0 +1,37 @@
+<?php
+$target_dir = "C:\Windows\Temp\php_image/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+   $finfo = finfo_open(FILEINFO_MIME_TYPE);
+$mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
+$ok = false;
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+        echo "Stored in: " . $target_file . "<br>";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
